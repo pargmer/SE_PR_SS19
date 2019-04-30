340ec87f79e2e2a8dc48d8fa727c82e250d1830c
kayjay05 <43863361+kayjay05@users.noreply.github.com>
2019-04-30 18:04:30 +0200

    Änderung bei CreateExerciseController
    GUI Änderung f. Erstellen neuer Übungen

--- SE_PR_Team04/src/controller/CreateExerciseController.java
+++ SE_PR_Team04/src/controller/CreateExerciseController.java
@@ -1,9 +1,8 @@
-/*
- * 
- */
 package controller;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,43 +12,46 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import model.Exercise;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class CreateExerciseController.
- */
 public class CreateExerciseController {
 
-	/** The root. */
 	@FXML
 	private AnchorPane root;
 	
-	/** The tf muscle. */
 	@FXML
 	private TextField tf_name,tf_reps,tf_muscle;
 	
+	List<Exercise> exercises = new LinkedList<Exercise>();
+	String workoutname;
 	
-	
-	/**
-	 * Handle btn save exercise.
-	 *
-	 * @param event the event
-	 * @throws IOException Signals that an I/O exception has occurred.
-	 */
 	@FXML
 	private void handleBtn_saveExercise(ActionEvent event) throws IOException {
 		
-		/*  Stage oldStage;
+		
+		
+		Exercise newExercise = new Exercise(tf_name.getText().toString(), tf_muscle.getText().toString(),Integer.parseInt(tf_reps.getText().toString()));
+		exercises.add(newExercise);		
+		
+		  Stage oldStage;
           oldStage = (Stage)root.getScene().getWindow();
 
           FXMLLoader fxmlLoader = new FXMLLoader();
           fxmlLoader.setLocation(getClass().getResource("/view/Create_Workout.fxml"));
           Parent root2 = (Parent) fxmlLoader.load();
+          CreateWorkoutController create = fxmlLoader.getController();
+          create.setData(exercises, workoutname);
           Stage stage = new Stage();
           stage.setTitle("Create Workout!");
           stage.setScene(new Scene(root2));  
           stage.show();
-          oldStage.close();*/
+          oldStage.close();
+		
+	}
+	
+	public void setData(List<Exercise> oexercises, String oworkoutname) {
+		exercises = oexercises;
+		workoutname = oworkoutname;
 		
 	}
 }
