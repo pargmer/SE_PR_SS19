cbe7a42d65ccd55dfeaf63828273ca25db3b1587
pargmer <parganmerisa@gmail.com>
2019-05-02 09:17:19 +0200

    Update ReadAndWriteCSV_test.java
    testReadWorkoutsFromCsv geschrieben aber date ist falsch angelegt,,

--- SE_PR_Team04/src/JUnitTests/ReadAndWriteCSV_test.java
+++ SE_PR_Team04/src/JUnitTests/ReadAndWriteCSV_test.java
@@ -6,10 +6,12 @@ package JUnitTests;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import model.Exercise;
 import model.ReadAndWriteCSV;
 import model.Workout;
 
@@ -19,6 +21,12 @@ import model.Workout;
  */
 class ReadAndWriteCSV_test {
 
+	private static Exercise e1 = new Exercise("Liegestuetz","Trizeps",15); 
+	private static Exercise e2 = new Exercise("Klimmzüge","Trizeps",15);
+	private static Exercise e3 = new Exercise("Sit-Ups","Trizeps",15);
+	private static Exercise e4 = new Exercise("Kniebeugen","Trizeps",15);
+	private static Exercise e5 = new Exercise("Strecksprünge","Trizeps",15);
+	
 	
 	/**
 	 * Test method for {@link model.ReadAndWriteCSV#readWorkoutsFromCsv(java.lang.String)}.
@@ -26,14 +34,23 @@ class ReadAndWriteCSV_test {
 	@Test
 	final void testReadWorkoutsFromCsv() {
 		ReadAndWriteCSV rw = new ReadAndWriteCSV();
-		List<Workout> workout = new List<Workout> 
+		List<Exercise> el = new LinkedList<Exercise>();
+		el.add(e1); el.add(e2); el.add(e3);
+		Workout w1 = new Workout("Oberkörpertraining", "Thu May 02 07:29:00 CEST 2019", el);
+		List<Exercise> e = new LinkedList<Exercise>();
+		e.add(e4); e.add(e5);
+		Workout w2 = new Workout("Beine", "Thu May 02 07:29:00 CEST 2019", e);
+		List<Workout> w = new LinkedList<Workout>();
+		w.add(w1); w.add(w2);
+		List<Workout> workouts;
 		try {
-			List<Workout> workouts = rw.readWorkoutsFromCsv("workouts.csv");
+			workouts = rw.readWorkoutsFromCsv("workouts.csv");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
+			fail("Something went wrong!");
 		}
-		fail("Not yet implemented"); // TODO
+		assertEquals(workouts, w);
 	}
 
 	/**
