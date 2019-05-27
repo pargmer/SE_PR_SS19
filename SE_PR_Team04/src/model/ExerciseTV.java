b1b6e5c17939ee653272916ed5da45ff4e08efc5
ElisabethEmp <lilli.e@gmx.at>
2019-05-27 20:04:06 +0200

    Create ExerciseTV.java
    ExerciseTV erstellt

---
+++ SE_PR_Team04/src/model/ExerciseTV.java
@@ -0,0 +1,58 @@
+package model;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.control.CheckBox;
+
+
+public class ExerciseTV {
+    
+    private String name;
+	private String trains;
+	private int reps;
+	private CheckBox active;
+
+    public ExerciseTV(String sname, String strains, int sreps, boolean active) {
+        this.name = sname;
+        this.trains =  strains;
+        this.reps = sreps;
+        this.active = new CheckBox();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTrains() {
+        return trains;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTrains(String trains) {
+        this.trains = trains;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+   
+
+    public CheckBox getActive() {
+        return active;
+    }
+
+    public void setActive(CheckBox active) {
+        this.active = active;
+    }
+
+  
+  
+}
