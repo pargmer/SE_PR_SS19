1e036df00244ccf8d0c76db2a5cb99206360ca9e
pargmer <parganmerisa@gmail.com>
2019-06-05 10:09:20 +0200

    update ControllerTEst

--- SE_PR_Team04/test/controllerTest/ControllerTest.java
+++ SE_PR_Team04/test/controllerTest/ControllerTest.java
@@ -5,28 +5,23 @@ import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.testfx.api.FxAssert;
-import org.testfx.api.FxRobot;
-import org.testfx.framework.junit5.ApplicationExtension;
-import org.testfx.framework.junit5.Start;
-import org.testfx.matcher.control.LabeledMatchers;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-@ExtendWith(ApplicationExtension.class)
+//@ExtendWith(ApplicationExtension.class)
 class ControllerTest {
 
 	private Button button;
-
 	/**
 	 * Will be called with {@code @Before} semantics, i. e. before each test method.
 	 *
 	 * @param stage - Will be injected by the test runner.
 	 */
-	@Start
+	//@Start
+/*
 	private void start(Stage stage) {
 		button = new Button("click me!");
 		button.setId("myButton");
@@ -35,9 +30,7 @@ class ControllerTest {
 		stage.show();
 	}
 
-	/**
-	 * @param robot - Will be injected by the test runner.
-	 */
+
 	@Test
 	void should_contain_button_with_text(FxRobot robot) {
 		FxAssert.verifyThat(button, LabeledMatchers.hasText("click me!"));
@@ -47,9 +40,7 @@ class ControllerTest {
 		FxAssert.verifyThat(".button", LabeledMatchers.hasText("click me!"));
 	}
 
-	/**
-	 * @param robot - Will be injected by the test runner.
-	 */
+	
 	@Test
 	void when_button_is_clicked_text_changes(FxRobot robot) {
 		// when:
@@ -61,7 +52,7 @@ class ControllerTest {
 		FxAssert.verifyThat("#myButton", LabeledMatchers.hasText("clicked!"));
 		// or (lookup by css class):
 		FxAssert.verifyThat(".button", LabeledMatchers.hasText("clicked!"));
-	}
+	}*/
 
 	@BeforeEach
 	void setUp() throws Exception {
