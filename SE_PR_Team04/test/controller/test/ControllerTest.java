5e5c8a5eebfb21c2ce6c1cf8b63bb5ff63f5efa4
kayjay05 <43863361+kayjay05@users.noreply.github.com>
2019-06-05 23:12:32 +0200

    Update ControllerTest.java

---
+++ SE_PR_Team04/test/controller/test/ControllerTest.java
@@ -0,0 +1,21 @@
+package controller.test;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+
+class ControllerTest {
+
+	@Before
+	public void setupStage() throws Throwable{
+	    showNodeInStage(null);
+	}
+	 
+	private void showNodeInStage(Object object){
+	    showNodeInStage(null);
+	}
+	
+	
+}
